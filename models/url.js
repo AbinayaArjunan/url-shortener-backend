3cc82d8110a308bf1120cbca391a873d6c55220e
abinayaarjunan <abiharsha2000@gmail.com>
2023-10-05 14:22:59 +0530

    commit

---
+++ models/url.js
@@ -0,0 +1,170 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema(
+    {
+        shortId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        shortURL: {
+            type: String,
+            required: true,
+        },
+        longURL: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: String,
+            required: true,
+        },
+        month: {
+            type: String,
+            required: true,
+        },
+        clicks: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        user: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+);
+
+module.exports = mongoose.model('Url', urlSchema, 'urls');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
